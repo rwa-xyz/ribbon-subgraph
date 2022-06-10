8bf143aa93963e87a179a2bad4e0b6cb380b6ac0
Steven <92332853+stevenwal@users.noreply.github.com>
2022-06-10 22:55:57 +0800

    feat: support swap contract (#70)

--- auctions/src/handlers.ts
+++ auctions/src/handlers.ts
@@ -1,4 +1,5 @@
-import { BigInt } from "@graphprotocol/graph-ts";
+import { BigInt, ByteArray, Bytes } from "@graphprotocol/graph-ts";
+import { NewOffer, SettleOffer, Swap, SwapContract } from "../../v2/generated/RibbonSwap/SwapContract";
 import {
   AuctionCleared,
   NewAuction,
@@ -114,4 +115,57 @@ export function handleNewUser(event: NewUser): void {
   user.address = event.params.userAddress
 
   user.save()
+}
+
+export function handleNewOffer(event: NewOffer): void {
+  let vault = event.transaction.to.toHexString()
+  let vaultExist = vaultList.includes(vault)
+
+  if (vaultExist) {
+    let auction = new Auction(event.params.swapId.toString())
+    let option = createOption(event.params.oToken)
+    let token = createToken(event.params.biddingToken)
+    
+    auction.option = option.id
+    auction.bidding = token.id
+    auction.minimum = event.params.minPrice
+    auction.size = event.params.totalSize
+    auction.start = event.block.timestamp
+    auction.end = BigInt.fromI32(0)
+    auction.filled = BigInt.fromI32(0)
+    auction.clearing = Bytes.fromHexString("") as Bytes
+    auction.live = true
+
+    auction.save()
+  }
+}
+
+export function handleSwap(event: Swap): void {
+  let swap = Auction.load(event.params.swapId.toString());
+
+  if (swap == null) {
+    return
+  }
+
+  swap.filled = swap.filled.plus(event.params.sellerAmount);
+  swap.save();
+}
+
+export function handleSettleOffer(event: SettleOffer): void {
+  let auction = Auction.load(event.params.swapId.toString())
+
+  if (auction) {
+    let Swap = SwapContract.bind(event.address)
+    let clearingPrice = Swap.averagePriceForOffer(event.params.swapId)
+    let clearingHex = clearingPrice.toHexString()
+    let len = clearingHex.length
+    clearingHex = len % 2 == 0 
+      ? clearingHex
+      : clearingHex.substring(0, 2) + "0" + clearingHex.substring(2) 
+    auction.clearing = Bytes.fromHexString(clearingHex) as Bytes
+    auction.end = event.block.timestamp
+    auction.live = false
+
+    auction.save()
+  }
 }
