5b49e4f0b13097bb8485fc2412c0b91da5eddf3d
jhh <cjinghong@users.noreply.github.com>
2022-07-05 10:48:21 +0300

    Added bal treasury subgraph (#78)

--- auctions/src/data/constant.ts
+++ auctions/src/data/constant.ts
@@ -4,11 +4,14 @@ export const vaultList: Array<string> = [
   "0x53773e034d9784153471813dacaff53dbbb78e8c", //RibbonThetaVaultSTETHCall
   "0x65a833afdc250d9d38f8cd9bc2b1e3132db13b2f", //RibbonThetaVaultWBTCCall
   "0xcc323557c71c0d1d20a1861dc69c06c5f3cc9624", //RibbonThetaYearnVaultETHPut
-  "0xe44edf7ad1d434afe3397687dd0a914674f2e405", //RibbonTreasuryVaultPERP
   "0x98d03125c62dae2328d9d3cb32b7b969e6a87787", //RibbonThetaVaultAVAXCall
   "0x9dd6be071b4292cc88b8190ab718329adea3e3a3", //RibbonThetaVaultAVAXPut
   "0x6bf686d99a4ce17798c45d09c21181fac29a9fb3", //RibbonThetaVaultSAVAXCall
   "0xa1da0580fa96129e753d736a5901c31df5ec5edf", //RibbonThetaVaultRETHCall
   "0xc0cf10dd710aefb209d9dc67bc746510ffd98a53", //RibbonThetaVaultAPECall
-  "0x3d1d40101e185e1fe3aedcf05fe2f5ba0af1f25d" //Kovan Gnosis
+  "0x3d1d40101e185e1fe3aedcf05fe2f5ba0af1f25d", //Kovan Gnosis
+
+  // Treasury
+  "0xe44edf7ad1d434afe3397687dd0a914674f2e405", //RibbonTreasuryVaultPERP
+  "0x2a6b048eb15c7d4ddca27db4f9a454196898a0fe", //RibbonTreasuryVaultBAL
 ];
